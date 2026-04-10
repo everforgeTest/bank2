4de5a81c1c58660d187f87f74372ed7367d4e43b
everforgeTest <everforgeapiautomation@gmail.com>
2026-04-10 14:00:59 +0530

    Update Bank.Controller.js

--- src/Controllers/Bank.Controller.js
+++ src/Controllers/Bank.Controller.js
@@ -29,5 +29,25 @@ class BankController {
         }
     }
 }
+async handleRequest(isReadOnly) {
+        switch (this.message.Action) {
+            case "Deposit":
+                if (isReadOnly) return { error: { code: 403, message: "Deposit not allowed in readonly." } };
+                return await this.service.deposit();
+            case "Withdraw":
+                if (isReadOnly) return { error: { code: 403, message: "Withdraw not allowed in readonly." } };
+                return await this.service.withdraw();
+            case "Transfer":
+                if (isReadOnly) return { error: { code: 403, message: "Transfer not allowed in readonly." } };
+                return await this.service.transfer();
+            case "GetBalance":
+                return await this.service.getBalance();
+            case "GetTransactions":
+                return await this.service.getTransactions();
+            default:
+                return { error: { code: 400, message: "Invalid action." } };
+        }
+    }
+}
 
 module.exports = { BankController };
